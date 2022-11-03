286b3fd8807728832acf3cb67675d47102d58210
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-03 16:02:49 +0100

    correct writing and reading of file

--- bufu stats.py
+++ bufu stats.py
@@ -3,28 +3,37 @@
 # use https://curlconverter.com/ and copy bash cURL for getting login cookies
 # login through headers and cookies because other approach does not work 
 
-import requests, re
-"""
+import requests, re, os, sys
+print(os.getcwd())
+os.chdir(os.path.dirname(sys.argv[0]))
+print(os.getcwd())
+
 #open and convert files with headers and cookies
 cookies = eval(open('cookies.txt').read())
 headers = eval(open('headers.txt').read())
 
 # TODO set params for practice part of forum; f is practice forum t is for threads - part of the loop
 
-params = {
-    'f': '11', 
-    't': '1275',
-}
-
-#params=params
-
 # TODO create loop for going throug all practice pages and append it to file
-page = requests.get('https://forum.brno-ultimate.cz/domains/forum.brno-ultimate.cz/viewtopic.php?', params=params, cookies=cookies, headers=headers)
+startT= 1280
+endtT= 1296
 
-#print(page.text)
-file = open("scraped.txt", "a")
-file.write('\n\n\n###   ' + page.url + '   ###\n\n\n' + page.text)
-"""
+#promaze puvodni soubor
+with open('scraped.txt', 'w', encoding='utf-8') as file:
+        file.write('')
+
+#TODO this is not working - need to get urls first
+
+for p in range(startT,endtT+1):
+    params = {
+        'f': '11', 
+        't': p,
+    }
+    page = requests.get('https://forum.brno-ultimate.cz/domains/forum.brno-ultimate.cz/viewtopic.php?', params=params, cookies=cookies, headers=headers)
+
+    with open('scraped.txt', 'a', encoding='utf-8') as file:
+        file.write('\n\n\n\   ###   ' + page.url + '   ###   \n\n\n' + page.text)
+
 
 #go through the file and count attendace (create dictionary)
 
@@ -32,16 +41,16 @@ membersInAnoRegex = re.compile(r'poll_vote_notice.+?(Ano.+?)Ne</label>', re.DOTA
 namesRegex = re.compile(r'''class="username">(.+?)</a>''')  #regex for finding names the result
 evidence = {}
 
-file = open("scraped.txt", "r")
-correctPart = membersInAnoRegex.findall(file.read())
-for training in correctPart:
-    attendants = namesRegex.findall(training)
-    for at in attendants:
-        evidence.setdefault(at,0)
-        evidence[at] += 1
+with open('scraped.txt', 'r', encoding='utf-8') as file:
+    correctPart = membersInAnoRegex.findall(file.read())
+    for training in correctPart:
+        attendants = namesRegex.findall(training)
+        for at in attendants:
+            evidence.setdefault(at,0)
+            evidence[at] += 1
 print(evidence)
-
+#os.remove('scraped.txt')
 
 # TODO save attendace to some nice format for sheet
 
-#file.close()
+#file.close() 
