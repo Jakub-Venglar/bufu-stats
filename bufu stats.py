438b0a5df7e1d604143caaf95e6bb6c584eb364d
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-03 13:30:22 +0100

    regexes and evidence counting solved

--- bufu stats.py
+++ bufu stats.py
@@ -3,7 +3,7 @@
 # use https://curlconverter.com/ and copy bash cURL for getting login cookies
 # login through headers and cookies because other approach does not work 
 
-import requests
+import requests, re
 
 #open and convert files with headers and cookies
 cookies = eval(open('cookies.txt').read())
@@ -13,7 +13,7 @@ headers = eval(open('headers.txt').read())
 
 params = {
     'f': '11', 
-    't': '1293',
+    't': '1272',
 }
 
 #params=params
@@ -21,14 +21,25 @@ params = {
 # TODO create loop for going throug all practice pages and append it to file
 page = requests.get('https://forum.brno-ultimate.cz/domains/forum.brno-ultimate.cz/viewtopic.php?', params=params, cookies=cookies, headers=headers)
 
-print(page.url)
-
-'''
-file = open("scraped.txt", "a")
+#print(page.text)
+file = open("scraped.txt", "w")
 file.write(page.text)
-'''
+
 
 # TODO go through the file and count attendace (create dictionary)
+
+membersInAnoRegex = re.compile(r'(poll_vote_notice.+)(Ano.+)(Ne</label>)', re.DOTALL)
+file = open("scraped.txt", "r")
+correctPart = membersInAnoRegex.search(file.read()).group()
+namesRegex = re.compile(r'''class="username">(.+?)</a>''')
+attendants = namesRegex.findall(correctPart)
+evidence = {}
+for at in attendants:
+    evidence.setdefault(at,0)
+    evidence[at] += 1
+print(evidence)
+
+
 # TODO save attendace to some nice format for sheet
 
 #file.close()
