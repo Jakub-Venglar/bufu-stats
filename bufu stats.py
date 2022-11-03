9a5efbe3de5eddb0aca3348e8a2f92dd6884e799
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-03 16:38:42 +0100

    finishing and debugging main loop

--- bufu stats.py
+++ bufu stats.py
@@ -4,25 +4,27 @@
 # login through headers and cookies because other approach does not work 
 
 import requests, re, os, sys
-print(os.getcwd())
+
+#make sure working directory is set the same as file directory
 os.chdir(os.path.dirname(sys.argv[0]))
-print(os.getcwd())
 
 #open and convert files with headers and cookies
 cookies = eval(open('cookies.txt').read())
 headers = eval(open('headers.txt').read())
 
-# TODO set params for practice part of forum; f is practice forum t is for threads - part of the loop
+# start and end parametr of t in url - f is for higher forum places and actually is not needed
 
-# TODO create loop for going throug all practice pages and append it to file
-startT= 1280
-endtT= 1296
+startT= 1238
+endtT= 1275
 
-#promaze puvodni soubor
+#make original file clean
 with open('scraped.txt', 'w', encoding='utf-8') as file:
         file.write('')
 
-#TODO this is not working - need to get urls first
+isPracticeRegex= re.compile(r'''(Pondělí |Čtvrtek ).+?(ZAČÁTEČNÍCI|POKROČILÍ|VŠICHNI|všichni)''') #regex for making sure we are in practice part
+membersInAnoRegex = re.compile(r'poll_vote_notice.+?(Ano.+?)Ne</label>', re.DOTALL) #regex for finding part of scraped mess where people voted as Ano
+
+#main loop for going throug all practice pages and append scraped content to file
 
 for p in range(startT,endtT+1):
     params = {
@@ -30,14 +32,14 @@ for p in range(startT,endtT+1):
         't': p,
     }
     page = requests.get('https://forum.brno-ultimate.cz/domains/forum.brno-ultimate.cz/viewtopic.php?', params=params, cookies=cookies, headers=headers)
-
-    with open('scraped.txt', 'a', encoding='utf-8') as file:
-        file.write('\n\n\n\   ###   ' + page.url + '   ###   \n\n\n' + page.text)
+    confirmPractice = isPracticeRegex.search(page.text)
+    if confirmPractice:         #make sure we are at the right place
+        with open('scraped.txt', 'a', encoding='utf-8') as file:
+            file.write('\n\n\n   ###   ' + page.url + '   ###   \n\n\n' + '\n\n\n   ###   ' + confirmPractice.group() + '   ###   \n\n\n' + page.text)
 
 
 #go through the file and count attendace (create dictionary)
 
-membersInAnoRegex = re.compile(r'poll_vote_notice.+?(Ano.+?)Ne</label>', re.DOTALL) #regex for finding part of scraped mess where people voted as Ano
 namesRegex = re.compile(r'''class="username">(.+?)</a>''')  #regex for finding names the result
 evidence = {}
 
@@ -49,8 +51,5 @@ with open('scraped.txt', 'r', encoding='utf-8') as file:
             evidence.setdefault(at,0)
             evidence[at] += 1
 print(evidence)
-#os.remove('scraped.txt')
 
 # TODO save attendace to some nice format for sheet
-
-#file.close() 
