425833bdc30cfc82cd5c00112043eaccfc7bc0a8
Jakub-Venglar <jakub.venglar@seznam.cz>
2022-11-07 17:11:56 +0100

    saving just important part from scraped page

--- bufu stats.py
+++ bufu stats.py
@@ -8,8 +8,11 @@ import requests, re, os, sys, csv
 #make sure working directory is set the same as file directory
 os.chdir(os.path.dirname(sys.argv[0]))
 
-isPracticeRegex= re.compile(r'''(Pondělí |Čtvrtek ).+?(ZAČÁTEČNÍCI|POKROČILÍ|VŠICHNI|všichni)''') #regex for making sure we are in practice part
+# regexes
+
+isPracticeRegex= re.compile(r'''(Pondělí |Čtvrtek ).+?(ZAČÁTEČNÍCI|POKROČILÍ|VŠICHNI|všichni)''') #regex for making sure we are in practice page
 membersInAnoRegex = re.compile(r'poll_vote_notice.+?(Ano.+?)Ne</label>', re.DOTALL) #regex for finding poll part of scraped mess where people voted as Ano - needs to be non-greedy
+namesRegex = re.compile(r'''class="username">(.+?)</a>''')  #regex for finding names the result
 datesRegex = re.compile(r'(([0-2]?[0-9]|3[0-1])\.([1-2]?[0-9])\.)')
 
 #open and convert files with headers and cookies
@@ -37,28 +40,27 @@ for p in range(startT,endtT+1):
     confirmPractice = isPracticeRegex.search(page.text)
     if confirmPractice:         #make sure we are at the right place
         with open('scraped.txt', 'a', encoding='utf-8') as file:
-            file.write('\n\n\n   ###   ' + page.url + '   ###   \n\n\n' + '\n\n\n   ###   ' + confirmPractice.group() + '   ###   \n\n\n' + page.text)
-            date = datesRegex.search(confirmPractice.group()) # also count dates so we how how many practices we had
+            toWrite = membersInAnoRegex.search(page.text) # now we are writing just important part
+            file.write('\n\n\n   ###START   ' + page.url + '   ###   \n\n\n' + '\n\n\n   ###   ' + confirmPractice.group() + '   ###   \n\n\n' + toWrite.group() + '   ###END   \n\n\n')
+            date = datesRegex.search(confirmPractice.group()) # also count dates so we now how many practices we had
             if date.group() not in listOfDates:
                 listOfDates.append(date.group())
 
-#go through the file and count attendace in dictionary
+#go through the file and count attendace into a dictionary and sort it
 
-namesRegex = re.compile(r'''class="username">(.+?)</a>''')  #regex for finding names the result
 evidence = {}
-#totalPractices = 0
 
 with open('scraped.txt', 'r', encoding='utf-8') as file:
-    correctPart = membersInAnoRegex.findall(file.read())
-    
+    correctPart = membersInAnoRegex.findall(file.read()) #could be more elegant but it is easy to reuse it for iteration
     for training in correctPart:
         attendants = namesRegex.findall(training)
-       # totalPractices += 1
         for at in attendants:
             evidence.setdefault(at,0)
             evidence[at] += 1
 
 sortedEvidence = dict(sorted(evidence.items(), key=lambda x:x[1], reverse=True))
+
+# print result
 
 print('Celkem tréninků: ' + str(len(listOfDates)) + '\n')
 
